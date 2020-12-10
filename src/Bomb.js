baaf14751e5b6bc175d8ae8c921cca8c67c23959
ChippyChoppy <rebeccamargaret13@gmail.com>
2020-12-10 00:40:41 -0500

    donezo

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,24 @@
-// your Bomb code here!
+import React from 'react'
+
+class Bomb extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { secondsLeft: props.initialCount }
+        console.log(this.state)
+    }
+
+    render() {
+        
+        if (this.state.secondsLeft === 0) {
+            return (
+                <p>Boom!</p>
+            )
+        } else {
+            return (
+                <p>{this.state.secondsLeft} seconds left before I go boom!</p>
+            )
+        }
+    }
+} // class end
+
+export default Bomb
